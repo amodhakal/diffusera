7a218442b3bd4d5a4a38758d30cb6ab22cdf362e
Amodh Dhakal <amodhakal@gmail.com>
2025-11-14 23:09:59 -0500

    Draw a rectangle to the screen

---
+++ shaders/shaders.vert
@@ -0,0 +1,7 @@
+#version 330 core
+
+in vec3 inPosition;
+
+void main() {
+    gl_Position = vec4(inPosition, 1.0);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,164 @@
-#include <print>
+#define GL_SILENCE_DEPRECATION
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <OpenGL/gl.h>
+#include <cstdlib>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+#include <sys/types.h>
+
+void handleResizeCallback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+const std::string getFullFileContents(const char *filePath);
+
+constexpr auto VERTEX_PATH = "./shaders/shaders.vert";
+constexpr auto FRAGMENT_PATH = "./shaders/shaders.frag";
 
 int main() {
-  std::println("Hello, {}!", "name");
-  return 0;
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "Linterra", nullptr, nullptr);
+  if (!window) {
+    glfwTerminate();
+    throw std::runtime_error("Failed GLFW window creation");
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, handleResizeCallback);
+  if (glewInit() != GLEW_OK) {
+    throw std::runtime_error("Failed to initialize GLEW");
+  }
+
+  const std::string vertexShaderCodeRaw = getFullFileContents(VERTEX_PATH);
+  const std::string fragmentShaderCodeRaw = getFullFileContents(FRAGMENT_PATH);
+  const char *vertexShaderCode = vertexShaderCodeRaw.c_str();
+  const char *fragmentShaderCode = fragmentShaderCodeRaw.c_str();
+
+  uint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  uint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+  glShaderSource(vertexShader, 1, &vertexShaderCode, nullptr);
+  glShaderSource(fragmentShader, 1, &fragmentShaderCode, nullptr);
+
+  glCompileShader(vertexShader);
+  glCompileShader(fragmentShader);
+
+  int success;
+  char infoLog[512];
+
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+    throw std::runtime_error("Vertex compilation: " + std::string(infoLog));
+  }
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+    throw std::runtime_error("Fragment compilation: " + std::string(infoLog));
+  }
+
+  unsigned int shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    throw std::runtime_error("Shader linking: " + std::string(infoLog));
+  }
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+  glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+  float vertices[] = {
+      0.5f,  0.5f,  0.0f, // top right
+      0.5f,  -0.5f, 0.0f, // bottom right
+      -0.5f, -0.5f, 0.0f, // bottom left
+      -0.5f, 0.5f,  0.0f  // top left
+  };
+
+  uint indices[] = {
+      0, 1, 3, // first triangle
+      1, 2, 3  // second triangle
+  };
+
+  // Used to manage everything (VBO, EBO) in 1 place
+  uint VAO;
+  glGenVertexArrays(1, &VAO);
+
+  // Start the configuration
+  glBindVertexArray(VAO);
+
+  // Used to store vertex info to be sent to the gpu
+  uint VBO;
+  glGenBuffers(1, &VBO);              // Creates a buffer on the gpu
+  glBindBuffer(GL_ARRAY_BUFFER, VBO); // Binds to VAO
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
+               GL_STATIC_DRAW); // Copy info from CPU to GPU
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+                        (void *)0); // Tell how vertex info is configured
+
+  // Used to store the indices to setup the vertices to create triangle
+  uint EBO;
+  glGenBuffers(1, &EBO);                      // Creates a buffer on the gpu
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // Binds to VAO
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW); // Copy info from CPU to GPU
+
+  // Stop the configuration
+  glEnableVertexAttribArray(0);
+
+  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glUseProgram(shaderProgram); // Use the defined shaders in this program
+    glBindVertexArray(VAO);      // Get ready to draw with this VAO
+    glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT,
+                   0); // Draw the objects
+
+    glfwSwapBuffers(window); // Swap the buffer
+    glfwPollEvents();        // Get any events if avaialbe
+  }
+
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
+  glDeleteProgram(shaderProgram);
+
+  glfwTerminate();
+  return EXIT_SUCCESS;
 }
 
+void handleResizeCallback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+const std::string getFullFileContents(const char *filePath) {
+  std::ifstream file(filePath);
+  if (!file.is_open()) {
+    throw std::runtime_error("Couldn't open file: " + std::string(filePath));
+  }
+
+  std::string content, line;
+  while (getline(file, line)) {
+    content += line + "\n";
+  }
+
+  return content;
+}
