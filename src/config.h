6fa53db7a4c28b3356b4e0033264d2514bdd89e4
Amodh Dhakal <amodhakal@gmail.com>
2025-11-17 12:05:47 -0500

    Created a chunk block representation

--- src/config.h
+++ src/config.h
@@ -35,7 +35,7 @@ constexpr glm::vec3 DEFAULT_UP = {0, 1, 0};
 namespace Chunk {
 constexpr int LENGTH = 16;
 constexpr int HEIGHT = 256;
-constexpr int RENDER_DISTANCE_CHUNKS = 8;
+constexpr int RENDER_DISTANCE_CHUNKS = 3;
 constexpr int RENDER_DISTANCE_BLOCKS = RENDER_DISTANCE_CHUNKS * LENGTH;
 }  // namespace Chunk
 
