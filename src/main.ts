787924b5212fd0668a4a4495179e4ad241ba7b0b
Amodh Dhakal <amodhakal@gmail.com>
2026-04-23 16:50:00 -0400

    feat: setup webgpu with basic triangle

---
+++ src/main.ts
@@ -0,0 +1,76 @@
+async function main() {
+  const adapter = await navigator.gpu.requestAdapter();
+  const device = await adapter.requestDevice();
+
+  const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
+  const context = canvas.getContext("webgpu");
+  const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+  context.configure({
+    device,
+    format: presentationFormat,
+  });
+
+  const module = device.createShaderModule({
+    label: "our hardcoded red triangle shaders",
+    code: /* wgsl */ `
+      @vertex fn vs(
+        @builtin(vertex_index) vertexIndex : u32
+      ) -> @builtin(position) vec4f {
+        let pos = array(
+          vec2f( 0.0,  0.5),  // top center
+          vec2f(-0.5, -0.5),  // bottom left
+          vec2f( 0.5, -0.5)   // bottom right
+        );
+ 
+        return vec4f(pos[vertexIndex], 0.0, 1.0);
+      }
+ 
+      @fragment fn fs() -> @location(0) vec4f {
+        return vec4f(1.0, 0.0, 0.0, 1.0);
+      }
+    `,
+  });
+
+  const pipeline = device.createRenderPipeline({
+    label: "our hardcoded red triangle pipeline",
+    layout: "auto",
+    vertex: {
+      module,
+    },
+    fragment: {
+      module,
+      targets: [{ format: presentationFormat }],
+    },
+  });
+
+  const renderPassDescriptor = {
+    label: "our basic canvas renderPass",
+    colorAttachments: [
+      {
+        clearValue: [0.3, 0.3, 0.3, 1],
+        loadOp: "clear",
+        storeOp: "store",
+      },
+    ],
+  };
+
+  // Get the current texture from the canvas context and
+  // set it as the texture to render to.
+  renderPassDescriptor.colorAttachments[0].view = context
+    .getCurrentTexture()
+    .createView();
+
+  // make a command encoder to start encoding commands
+  const encoder = device.createCommandEncoder({ label: "our encoder" });
+
+  // make a render pass encoder to encode render specific commands
+  const pass = encoder.beginRenderPass(renderPassDescriptor);
+  pass.setPipeline(pipeline);
+  pass.draw(3); // call our vertex shader 3 times
+  pass.end();
+
+  const commandBuffer = encoder.finish();
+  device.queue.submit([commandBuffer]);
+}
+
+window.onload = main;
